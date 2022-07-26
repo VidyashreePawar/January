d95dc8092b030e914f3c9cccae1305c2bdb868d0
Moraya <Moraya@DESKTOP-730UPJ9>
2022-07-26 10:56:38 +0530

    2nd commit

--- src/com/Vidya/Github/Sample.java
+++ src/com/Vidya/Github/Sample.java
@@ -3,6 +3,10 @@ package com.Vidya.Github;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println(sum);
 	}
 
 }
---
+++ src/com/Vidya/Github/Sample2.java
@@ -0,0 +1,12 @@
+package com.Vidya.Github;
+
+public class Sample2
+{
+public static void main(String[] args) 
+{
+	int p = 50;
+	int q = 30;
+	int sub = p-q;
+	System.out.println(sub);
+}
+}
